c18b6ec2b2b25b3a87123cf61c1e74f8d15d4b88
marcogalantee <115880323+marcogalantee@users.noreply.github.com>
2024-10-06 12:34:03 +0200

    aggiornato tutta la parte sotto

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,15 @@ document.getElementById('burger').addEventListener('click', function () {
 document.getElementById('chiudi').addEventListener('click', function () {
     aggiungiClasse('menu', 'active');
 });
+
+
+
+function showPreview(button) {
+    const preview = button.nextElementSibling; // Seleziona la .video-preview
+    preview.style.display = "block"; // Mostra l'anteprima video
+}
+
+function hidePreview(button) {
+    const preview = button.nextElementSibling;
+    preview.style.display = "none"; // Nascondi l'anteprima video
+}
